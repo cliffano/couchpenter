4097bec055ba72db3fffea251d53f7673677d9c4
Cliffano Subagio <cliffano@gmail.com>
2012-09-02 13:50:37 +1000

    Add couchpenter tasks-related tests.

--- test/couchpenter.js
+++ test/couchpenter.js
@@ -47,5 +47,93 @@ describe('couchpenter', function () {
       checks.console_log_messages[0].should.equal('Creating sample configuration file: couchpenter.json');
     });
   });
+
+  describe('task', function () {
+
+    it('should execute db task functions', function (done) {
+      checks.db_foo_call_counts = 0;
+      checks.db_bar_call_counts = 0;
+      mocks.requires = {
+        './db': function (url, config, dir) {
+          checks.db_url = url;
+          checks.db_config = config;
+          checks.db_dir = dir;
+          return {
+            foo: function(cb) {
+              checks.db_foo_call_counts++;
+              cb();
+            },
+            bar: function(cb) {
+              checks.db_bar_call_counts++;
+              cb();
+            }
+          };
+        }
+      };
+      couchpenter = new (create(checks, mocks))();
+      couchpenter.task(['foo', 'bar'], function () {
+        done();
+      });
+
+      checks.db_foo_call_counts.should.equal(1);
+      checks.db_bar_call_counts.should.equal(1);
+    });
+  });
+
+  describe('setUp', function () {
+
+    it('should execute db task functions', function (done) {
+      checks.db_setUpDatabases_call_counts = 0;
+      checks.db_setUpDocuments_call_counts = 0;
+      mocks.requires = {
+        './db': function (url, config, dir) {
+          checks.db_url = url;
+          checks.db_config = config;
+          checks.db_dir = dir;
+          return {
+            setUpDatabases: function(cb) {
+              checks.db_setUpDatabases_call_counts++;
+              cb();
+            },
+            setUpDocuments: function(cb) {
+              checks.db_setUpDocuments_call_counts++;
+              cb();
+            }
+          };
+        }
+      };
+      couchpenter = new (create(checks, mocks))();
+      couchpenter.setUp(function () {
+        done();
+      });
+      checks.db_setUpDatabases_call_counts.should.equal(1);
+      checks.db_setUpDocuments_call_counts.should.equal(1);
+    });
+  });
+
+  describe('tearDown', function () {
+
+    it('should execute db task functions', function (done) {
+      checks.db_tearDownDatabases_call_counts = 0;
+      mocks.requires = {
+        './db': function (url, config, dir) {
+          checks.db_url = url;
+          checks.db_config = config;
+          checks.db_dir = dir;
+          return {
+            tearDownDatabases: function(cb) {
+              checks.db_tearDownDatabases_call_counts++;
+              cb();
+            }
+          };
+        }
+      };
+      couchpenter = new (create(checks, mocks))();
+      couchpenter.tearDown(function () {
+        done();
+      });
+      checks.db_tearDownDatabases_call_counts.should.equal(1);
+    });
+  });
 });
  
